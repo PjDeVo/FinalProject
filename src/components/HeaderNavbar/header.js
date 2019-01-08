814c0661215c3bd27a091afc89d81513b751695f
Owner <owner@Users-MacBook-Pro.local>
2019-01-08 01:52:15 -0500

    replaced the placeholder on homepage and adjusted height of grid for title to compress page and make it look cleaner

--- src/components/HeaderNavbar/header.js
+++ src/components/HeaderNavbar/header.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 
 import history from '../../history';
+
 class Header extends Component {
     render() {
         return(
             <div className = 'header'>
-                <img className = 'header__image' onClick ={()=> history.push('./')} src = '../../../src/style/images/Grindhouse/jerry.jpg'/>
+                <img className = 'header__image' onClick ={()=> history.push('./')} src = 'http://via.placeholder.com/100x100'/>
             </div> 
             
         )
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -18,6 +18,12 @@ class Home extends Component {
                     content = {" Here is the content for the Muay Thai description on the homepage Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "}
                     image = { 'http://via.placeholder.com/300x300'}
                  />
+                 <HomeForm 
+                    className ={'home__form-announcements'}
+                    title = {'Announcements'} 
+                    content = {" Here is the content for the Announcement description on the homepage Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "}
+                    image = { 'http://via.placeholder.com/300x300'}
+                 />
             </div>
         )
     }
