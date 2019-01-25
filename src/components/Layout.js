7962cfa79e7a340f90078d09aaa4c34eec4c12ec
Owner <owner@Users-MacBook-Pro.local>
2019-01-24 22:17:18 -0500

    more modifiations for media queries home page should be fully responsive now to 320-640px

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,7 @@ class Layout extends Component {
         {this.props.children}
         <Header  />
         <NavigationContainer />
-        <Footer />
+        <Footer  />
       </div>
     );
   }
