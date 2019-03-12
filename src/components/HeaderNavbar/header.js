88602971e4c1e7b57ac02842d569c659399b6309
Owner <owner@Users-MacBook-Pro.local>
2019-03-11 21:58:04 -0400

    finished hamburger menu functionality in navbarContainer.js

--- src/components/HeaderNavbar/header.js
+++ src/components/HeaderNavbar/header.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 
 import history from '../../history';
 
+
+
 class Header extends Component {
     render() {
         return(
             <div className = 'header'>
+               
                 <img className = 'header__image' onClick ={()=> history.push('./')} src = {require('../../../static/assets/Grindhouse/logo18a-sm.png')}/>
             </div> 
             
--- src/components/HeaderNavbar/navigationContainer.js
+++ src/components/HeaderNavbar/navigationContainer.js
@@ -1,32 +1,59 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars)
 
 class NavigationContainer extends Component {
-  constructor() {
-    super()
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      isExpanded: false
+    }
   }
 
+ handleToggle(e) {
+  this.setState({
+    isExpanded: !this.state.isExpanded
+  })
+ }
+
   render() {
+    const {isExpanded} = this.state
 
-
+    
     return (
-      <div className="nav-wrapper">
+      <div>
+          <div className = 'hamburger-menu'>
+                      <FontAwesomeIcon className ={`mobile-menu` } icon="bars" onClick = {(e) => this.handleToggle(e)} />
+                  </div>
 
-          <div className = 'nav-link-wrapper__home'>
-            <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-          </div>
-          <div className = 'nav-link-wrapper__meet'>
-            <NavLink to="/meet-the-instructors" activeClassName="nav-link-active">Instructors</NavLink>
-          </div>
-          <div className = 'nav-link-wrapper__kids'>
-            <NavLink to="/kids" activeClassName="nav-link-activFuture Champe">Kids</NavLink>
-          </div>
-          <div className = 'nav-link-wrapper__schedule'>
-            <NavLink to="/schedule" activeClassName="nav-link-active">Schedule</NavLink>
-          </div>
-          <div className = 'nav-link-wrapper__contact'>
-            <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
-          </div>
+        <div className={`nav-wrapper ${isExpanded ? 'show' :  'hidden' }`}>
+        
+
+            <div className = 'nav-link-wrapper__home'>
+              <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+            </div>
+            <div className = 'nav-link-wrapper__meet'>
+              <NavLink to="/meet-the-instructors" activeClassName="nav-link-active">Instructors</NavLink>
+            </div>
+            <div className = 'nav-link-wrapper__kids'>
+              <NavLink to="/kids" activeClassName="nav-link-activFuture Champe">Kids</NavLink>
+            </div>
+            <div className = 'nav-link-wrapper__schedule'>
+              <NavLink to="/schedule" activeClassName="nav-link-active">Schedule</NavLink>
+            </div>
+            <div className = 'nav-link-wrapper__contact'>
+              <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+            </div>
+
+          
+        </div>
+
       </div>
     );
   }
