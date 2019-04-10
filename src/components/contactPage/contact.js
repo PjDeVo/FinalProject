d85e5aa503bc502e1ad1a38618bcc361b76f083f
Owner <owner@Users-MacBook-Pro.local>
2019-04-10 00:45:08 -0400

    fixed background color issue

--- src/components/contactPage/contact.js
+++ src/components/contactPage/contact.js
@@ -23,7 +23,7 @@ class Contact extends Component {
                     <ContactForm className = 'contact-form'/>
                     <Button className = 'contact-form__button' title = 'Submit'  />
                     <div className = 'contact__map'>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3049.1993604607846!2d-75.01531538429577!3d40.160114779129614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6ad9fe0b32bed%3A0x3a6dd747e6e637ac!2sGrindhouse+MMA+%26+Fitness!5e0!3m2!1sen!2sus!4v1548039573988" width="996" height="390" ></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3049.1993604607846!2d-75.01531538429577!3d40.160114779129614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6ad9fe0b32bed%3A0x3a6dd747e6e637ac!2sGrindhouse+MMA+%26+Fitness!5e0!3m2!1sen!2sus!4v1548039573988" width="796" height="390" ></iframe>
 
                     </div>    
                 </div>
@@ -33,7 +33,6 @@ class Contact extends Component {
                    
 
                     <div className = 'contact__phone-wrapper'>
-                        <FontAwesomeIcon className ='contact__phone'  icon="phone" />
                         <h1> Contact Info</h1>
                         <p>215-479-9662</p>
                         <p>ghousemma@gmail.com</p>
@@ -43,7 +42,6 @@ class Contact extends Component {
                 </div>    
                 <div className = 'contact__address-wrapper'>
                     <div className = 'contact__address'>
-                        <FontAwesomeIcon className ='contact__map-marker'  icon="map-marker-alt" />
                         <h1> Address</h1>
                         <p> Grindhouse MMA & Fitness</p>
                         <p> 1040 Mill Creek Drive</p>
